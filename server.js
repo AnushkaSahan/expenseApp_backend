ecc77fe9bdb1abc44a867404351554754e76fc6a
Anushka <anushkasahan200439@gmail.com>
2025-10-08 00:08:56 +0530

    Updated again

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+  res.send("It's Working");
+});
+
+console.log("My Port:", process.env.PORT);
+
+app.listen(5001, () => {
+  console.log("Server is up and running on PORT:", PORT);
+});
